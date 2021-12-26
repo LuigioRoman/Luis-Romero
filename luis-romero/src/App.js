b8d535a1c00a598226c6bb54a145e7d357332d11
Luis Romero <luisDevelop1@outlook.com>
2021-12-25 23:51:32 -0600

    Se muestra la lista de experiencia de cada uno de los puestos guardados en ListaExperiencias

--- luis-romero/src/App.js
+++ luis-romero/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import './components/experiencia';
 import Experiencias from './components/experiencia';
@@ -6,7 +5,10 @@ import Experiencias from './components/experiencia';
 function App() {
   return (
     <div className="App">
+      <button className="subtitulos"><h2>Experiencia</h2></button>
       <Experiencias/>
+      <button className="subtitulos"><h2>Educacion</h2></button>
+      <button className="subtitulos"><h2>Curriculum</h2></button>
     </div>
   );
 }
--- luis-romero/src/components/experiencia.js
+++ luis-romero/src/components/experiencia.js
@@ -4,64 +4,67 @@ import '../App.css';
 const Experiencias = () => {
     return(
         <>
-            <button className="subtitulos">
-                <h2>Mi Experiencia</h2>
-            </button>
-            <Lista />
+            <ListarExperiencias listaDeExperiencias={listaDeExperiencias} />
         </>
     )
 }
 
-const Lista = () => {
+const MostrarExperiencias = ({elementoDeExperiencia}) => {
+    
     return(
         <>
-            <h4>{listaDeExperiencias[0].titulo}</h4>
-            <div className='imagenContainerContent'></div>
-            <div className='container'>
-                <p className='content'>{listaDeExperiencias[0].empresa}</p>
-                <p className='content'>{listaDeExperiencias[0].responsabilidades}</p>
-                <p className='content'>{listaDeExperiencias[0].logros}</p>
+            <h4>{elementoDeExperiencia.titulo}</h4>
+            <div className='exp'>
+                <div className='imagenContainerContent exp-content'></div>
+                <div className='container exp-content'>
+                    <p className='content'>{elementoDeExperiencia.empresa}</p>
+                    <p className='content'>{elementoDeExperiencia.responsabilidades}</p>
+                    <p className='content'>{elementoDeExperiencia.logros}</p>
+                </div>
             </div>
             <hr/>
-            
-            <h4>{listaDeExperiencias[1].titulo}</h4>
-            <div className='container'>
-                <p className='content'>{listaDeExperiencias[1].empresa}</p>
-                <p className='content'>{listaDeExperiencias[1].responsabilidades}</p>
-                <p className='content'>{listaDeExperiencias[1].logros}</p>
-            </div>
-            <hr/>
-            
-            <h4>{listaDeExperiencias[2].titulo}</h4>
-            <div className='container'>
-                <p className='content'>{listaDeExperiencias[2].empresa}</p>
-                <p className='content'>{listaDeExperiencias[2].responsabilidades}</p>
-                <p className='content'>{listaDeExperiencias[2].logros}</p>
-            </div>
-            <hr/>
-        </>
+            </>
     )
 }
 
+const ListarExperiencias = ({listaDeExperiencias}) => {
+    return(
+        <div>
+            {listaDeExperiencias.map(elementoDeExperiencia =>(
+                <MostrarExperiencias key={elementoDeExperiencia.id} elementoDeExperiencia={elementoDeExperiencia}/>
+            ))}
+        </div>
+    )
+}
 
 const listaDeExperiencias = [
     {
-        titulo: "Puesto",
+        id: 1,
+        titulo: "Puesto 1",
         empresa: "Empresa, Contacto",
         responsabilidades: "Nulla imperdiet nisl enim, eu commodo augue iaculis et. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Mauris vel odio felis. Ut id quam egestas, viverra nunc et, rutrum velit. Ut dignissim mattis magna a malesuada. Quisque ultrices velit sem, eget lobortis ligula aliquet sed. Quisque interdum condimentum viverra. ",
         logros: "un logro, dos logros, tres logros",
     },
     {
-        titulo: "Puesto",
+        id: 2,
+        titulo: "Puesto 2",
         empresa: "Empresa, Contacto",
         responsabilidades: "Nulla imperdiet nisl enim, eu commodo augue iaculis et. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Mauris vel odio felis. Ut id quam egestas, viverra nunc et, rutrum velit. Ut dignissim mattis magna a malesuada. Quisque ultrices velit sem, eget lobortis ligula aliquet sed. Quisque interdum condimentum viverra. ",
         logros: "un logro, dos logros, tres logros",
     },
     {
-        titulo: "Puesto",
+        id:3,
+        titulo: "Puesto 3",
         empresa: "Empresa, Contacto",
         responsabilidades: "Nulla imperdiet nisl enim, eu commodo augue iaculis et. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Mauris vel odio felis. Ut id quam egestas, viverra nunc et, rutrum velit. Ut dignissim mattis magna a malesuada. Quisque ultrices velit sem, eget lobortis ligula aliquet sed. Quisque interdum condimentum viverra. a",
         logros: "un logro, dos logros, tres logros",
+    },
+    {
+        id:4,
+        titulo: "Puesto 4",
+        empresa: "Empresa, Contacto",
+        responsabilidades: "Nulla imperdiet nisl enim, eu commodo augue iaculis et. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Mauris vel odio felis. Ut id quam egestas, viverra nunc et, rutrum velit. Ut dignissim mattis magna a malesuada. Quisque ultrices velit sem, eget lobortis ligula aliquet sed. Quisque interdum condimentum viverra. a",
+        logros: "un logro, dos logros, tres logros, cuatro logros",
     },
 ]
 
